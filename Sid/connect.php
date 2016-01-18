102fef9f7708c36f47ad140cbd37b37b677026a5
sidwa <siddhantmreddy@gmail.com>
2016-01-18 15:00:11 +0530

    added everything

---
+++ Sid/connect.php
@@ -0,0 +1,24 @@
+<?php 
+$servername="localhost";
+$username="root";
+$password="";
+$db="test";
+
+$conn=new mysqli($servername,$username,$password,$db);
+if($conn->connect_error)
+{
+	die("connection error".$conn->connect_error);
+}
+else
+{
+}
+/*for($i=0;$i<100;$i++)
+{
+	for($j=0;$j<100;$j++)
+	{
+		$q="INSERT INTO grid (row,col)
+		VALUES ($i,$j)";
+		$conn->query($q);
+	}	
+}*/
+?>
--- Sid/time_trail.php
+++ Sid/time_trail.php
@@ -1,20 +1,37 @@
 <?php
-	include 'connect.php';
+	$servername="localhost";
+	$username="root";
+	$password="";
+	$db="test";
+	$conn=new mysqli($servername,$username,$password,$db);
+	if($conn->connect_error)
+	{
+		die("connection error".$conn->connect_error);
+	}
 	$sql="SELECT current FROM time WHERE 1";
 	$x=$conn->query($sql);
 	while($row=$x->fetch_assoc())
 	{
 		$old=$row['current'];
+		print_r($row);
 	}
-	$sql="SELECT TIMESTAMPDIFF(MINUTE,'$old','NOW()')";
+	//$sql="SELECT TIMESTAMPDIFF(MINUTE,'$old',NOW())";
+	$sql="SELECT COUNT(current) FROM time WHERE 1";
 	$y=$conn->query($sql);
-	if($res->num_rows>0)
+	if($y===false)
 	{
-		while($row =$y->fetch_assoc());
-		{
-			$result=$row['TIMESTAMPDIFF'];
-		}
+		echo "<br>".$conn->error."<br>";
 	}
-	echo $result;
+	else
+	{
+		if($y->num_rows>0)
+		{
+			while($row = $y->fetch_assoc());
+			{
+				print_r($row);
+			}
+		}
+	}	
+	//echo $result;
 	$conn->close();
 ?>
