9456803250b1d8dc14a799922d9f413591740709
Jacques Fernandes <jacques.fernandes.1337@gmail.com>
2016-01-20 15:04:03 +0530

    Sid's changes to player.php on my laptop

--- local-map/player.php
+++ local-map/player.php
@@ -1,18 +1,21 @@
 <?php
-
+require "../db_access/db.php"
 
 $playerid;
+$moveCostFood=100;
+$moveCostPower=50;
 /*$food=$water=$power=$metal=$wood;
 $food_regen=$water_regen=$power_regen=$metal_regen=$wood_regen;
 */
 function getStats(){
-	global $playerid, $dbconn;
+	global $dbconn;
 	
 	connect();
 	$playerid = 2; // $_SESSION["tek_emailid"];
 	setTable("player");
 
-	$query = "SELECT faction,food,food_regen,water,water_regen,power,power_regen,metal,metal_regen,wood,wood_regen,total FROM player WHERE tek_emailid=".$playerid.";";
+	$query = "SELECT faction,food,food_regen,water,water_regen,power,power_regen,metal,metal_regen,wood,wood_regen,total FROM player 
+	WHERE tek_emailid=".$playerid.";";
 	
 	$res = mysqli_query($dbconn,$query);
 	$res = mysqli_fetch_assoc($res);	
@@ -22,6 +25,31 @@ function getStats(){
 	disconnect();
 	return($res);
 }
-
-
+function max($x,$y)    //finds the greater of the two numbers
+{
+	if($x>$y)
+		return $x;
+	else
+		return $y;
+}
+function deductResource($resource,$value)   //use to reduce resource on some action give resource name and value  resp.
+{
+	$sql="UPDATE `player` SET $resource='$value' WHERE tek_emailid=$playerid";	
+	if($dbconn->query($sql)===false)
+	{
+		echo "error: ".dbconn->error;
+	}
+}
+function move($srcRow,$srcCol,$destRow,$destCol)
+{
+	$distance=max(abs($srcRow-$destRow),abs($srcCol-$destCol));
+	$foodCost=$distance*$moveCostFood;
+	$powerCost=$distnace*$moveCostPower;
+	deductResource("food",$foodCost);
+	deductResource("power",$powerCost);  
+}
+function settle() //occupies selected slot ***incomplete***
+{
+	
+}	
 ?>
