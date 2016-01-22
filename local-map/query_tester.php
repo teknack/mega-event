4267fe7bfc79da1a337645b1392ab648af99114f
sidwa <siddhantmreddy@gmail.com>
2016-01-22 23:19:23 +0530

    test folder is for adding all the code to be tested and more changes in player.php

--- local-map/query_tester.php
+++ local-map/query_tester.php
@@ -3,9 +3,10 @@
 	$row=0;
 	$col=8;
 	$row[8];
-	$col[8]
+
 	$sql="SELECT root FROM grid WHERE (row=$row-1 or row=$row or row=$row+1) and (col=$col-1 or col=$col or col=$col+1);";//add query
 	if($conn->query($sql)===false)
 		echo "no go : ".$conn->error;
 	else
 		echo "good to go";
+?>
