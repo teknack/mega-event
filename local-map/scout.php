f9315c57ca8ac0dcc7dbdd6a523e2d03d3718537
Jacques Fernandes <jacques.fernandes.1337@gmail.com>
2016-01-25 10:11:51 +0530

    trying out a new scout function

--- local-map/scout.php
+++ local-map/scout.php
@@ -11,4 +11,25 @@ function scout($row,$col) //Used to check the details of the column at $row,$col
 	return($slot);
 }
 
+function scoutv2($row,$col)
+{
+	connect();
+	setTable("grid");
+	$slot=getSlot($row,$col); //contains occupied,fortification,troops,faction
+	
+	if ($slot["occupied"] === $_SESSION["tek_emailid"])
+	{
+		alert("This is your own territory \n check your console");
+	}
+	else
+	{
+		alert("Check your console");
+	}
+		
+	consoleLog("Occupied : ".$slot["occupied"]);
+	consoleLog("Fortification : ".$slot["fortification"]);
+	consoleLog("Troops : ".$slot["troops"]);
+	consoleLog("Faction : ".$slot["faction"]);
+}
+
 ?>
