4c502b0c8c5e02c32b9aa6685bb5c9802399c466
Nischal Abraham <nischalabraham@gmail.com>
2016-02-12 16:27:12 +0530

    Experimenting

--- local-map/index.php
+++ local-map/index.php
@@ -12,6 +12,7 @@ session_start();
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+    <link href='https://fonts.googleapis.com/css?family=Josefin+Slab:400,700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" type="text/css" href="../maincss/mainstyle.css">	
 	<link rel="stylesheet" type="text/css" href="./css/local.css">
 	<title>Test page</title>
