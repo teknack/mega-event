f9f8e50cb209599d950a9db18d692a86a0a93a47
sidwa <siddhantmreddy@gmail.com>
2016-02-13 21:04:54 +0800

    Image compressed

---
+++ world-map/getLeader.php
@@ -0,0 +1,31 @@
+<?php
+	session_start();
+	$servername="localhost";
+	$username="root";
+	$password="";
+	$db="Mega";
+	$conn=new mysqli($servername,$username,$password,$db);
+	if($conn->connect_error)
+	{
+		die("connection error".$conn->connect_error);
+	}
+	$sql="SELECT tek_emailid,total,faction FROM player ORDER BY total DESC;";
+	$res=$conn->query($sql);
+	$row=$res->fetch_assoc();
+	$faction=$row['faction'];
+	$res= $conn->query($sql);
+	$troops=0;
+	$output="[";                                        //stores JSON string format [{occupied:"value",faction:"value"},
+	if($res->num_rows > 0)                              //                           {occupied:"value",faction:"value"}]
+	{
+		$i=0;
+		while($row = $res->fetch_assoc() and $i<5)
+		{
+			$output=$output.'{"user":"'.$row["tek_emailid"].'","faction":"'.$row["faction"].'","tiles":"'.$row['total'].'"},';
+			$i++;
+		}
+		$output.="{}]";
+	}
+	echo $output;
+	$conn->close();
+?>
