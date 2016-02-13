56123f1e606171f31496f455c1d254bf1736e29a
Jacques Fernandes <jacques.fernandes.1337@gmail.com>
2016-02-13 16:00:37 +0530

    meow

--- db_access/db.php
+++ db_access/db.php
@@ -31,8 +31,8 @@ session_start();
 //validate(); //VALIDATION CHECK
 
 $dbusername="root";
-$dbpassword="admin";
-$dbname="tek16_megaevent";
+$dbpassword="";//"admin";
+$dbname="Mega";//"tek16_megaevent";
 $dbtable="grid";
 $dbconn="";
 $dbservername="localhost";
